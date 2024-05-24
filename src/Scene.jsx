e95a48436cc57ebf095d81377bb499ed119dcb6c
utkarsh <khareu550@gmail.com>
2024-05-24 19:39:17 +0530

    First Commit

---
+++ src/Scene.jsx
@@ -0,0 +1,50 @@
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { Suspense } from "react";
+import { Car } from "./Car";
+import { Ground } from "./Ground";
+import { Track } from "./Track";
+import { useState, useEffect } from "react";
+
+export function Scene() {
+  const [thirdPerson, setThirdPerson] = useState(false);
+  const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21]);
+
+  useEffect(() => {
+    function keydownHandler(e){
+      if(e.key === "k") {
+        if(thirdPerson) {
+          setCameraPosition([-6, 3.9, 6.21 + Math.random() * 0.01]);
+        }
+        setThirdPerson(!thirdPerson);
+      }
+    }
+
+    window.addEventListener("keydown", keydownHandler);
+    return () => window.removeEventListener("keydown", keydownHandler)
+  }, [thirdPerson])
+
+  return (
+    <Suspense fallback={null}>
+      <Environment
+        files={process.env.PUBLIC_URL + "/textures/envmap.hdr"}
+        background={"both"}
+      />
+
+      <PerspectiveCamera makeDefault position={cameraPosition} fov={40} />
+      {
+        !thirdPerson && (
+          <OrbitControls target={[-2.64, -0.71, 0.03]} />
+        )
+
+      }
+
+      <Track />
+      <Ground />
+      <Car thirdPerson={thirdPerson} />
+    </Suspense>
+  );
+}
